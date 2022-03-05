31b98f89d6f41fc9e3019600c13589d0c7178c7f
Jafar7766 <jafarbakhromov@mail.ru>
2022-03-06 02:52:26 +0300

    Initial commit

---
+++ app/src/main/java/com/example/parcelable/SecondActivity.java
@@ -0,0 +1,34 @@
+package com.example.parcelable;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.parcelable.model.User;
+
+public class SecondActivity extends AppCompatActivity {
+    @SuppressLint("SetTextI18n")
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        TextView textView = new TextView(this);
+        textView.setTextSize(25);
+        textView.setPadding(22, 20, 20, 20);
+
+        Bundle arguments = getIntent().getExtras();
+
+        User user;
+        if(arguments!=null){
+            user = arguments.getParcelable(User.class.getSimpleName());
+
+            textView.setText("Name: " + user.getName() + "\nCompany: " + user.getCompany() +
+                    "\nAge: " + user.getAge());
+        }
+        setContentView(textView);
+
+    }
+}
+
